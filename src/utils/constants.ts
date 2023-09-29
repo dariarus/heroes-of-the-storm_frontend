139eefd6a420c2c1d8836e94445dc10cc49b8901
dariarus <rusanova_dv@mail.ru>
2023-09-29 18:26:07 +0300

    bugfix: сохранить статические картинки в проект в images. Настроить их отображение на главной (аватарки героев, роли)

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,11 +1,25 @@
+import abathurImage from '../images/mock_heroes/abathur.jpg';
+import azmodanImage from '../images/mock_heroes/azmodan.jpg';
+import alarakImage from '../images/mock_heroes/alarak.jpg';
+import alextraszaImage from '../images/mock_heroes/alextrasza.jpg';
+import anaImage from '../images/mock_heroes/ana.jpg';
+import anduinImage from '../images/mock_heroes/anduin.jpg';
+
+import support5 from '../images/mock_roles/Support5.png'
+import rdd2 from '../images/mock_roles/Rdd2.png'
+import mdd5 from '../images/mock_roles/Mdd5.png'
+import healer4 from '../images/mock_roles/Healer4.png'
+import healer5 from '../images/mock_roles/Healer5.png'
+import healer2 from '../images/mock_roles/Healer2.png'
+
 export const mockHeroesData = [
   {
     id: 1,
     name: "Абатур",
     role: "support",
     complexity: "very hard",
-    image: "https://docviewer.yandex.ru/view/682023045/htmlimage?id=7rzb-grzbyx0iccinkm3zh4n2bu2hldy0l2hk9w7zv4zeoj8xug8cb2edsl8utupkq6t27rzc51kvufmiv1neb9mbu1etjp4msq4izkv&name=image-ZPvnxSIooUnLcAnbyY.jpg&dsid=ce356ee44b262382114c2e4fdabac6bb",
-    icon: "https://docviewer.yandex.ru/view/682023045/htmlimage?id=1jd-iiukvn7hv68xyslifbqz7ekpmsk0lro0oui7hwu4ykrad0f13sg96xqghczfd8cgbfbd608s44mrsz5xnvttiv0sri47gydeiva&name=image-qpMxc7czs1uCX6qMON.png&dsid=ff1b4be15cd6d867bb6e7ea5444d078b",
+    image: abathurImage,
+    icon: support5,
     isInFreeRotation: true
   },
   {
@@ -13,8 +27,8 @@ export const mockHeroesData = [
     name: "Азмодан",
     role: "range-assassin",
     complexity: "easy",
-    image: "https://docviewer.yandex.ru/view/682023045/htmlimage?id=7xzf-hlmyx7ldd4iui6ywf8kkydy8rydqetsuy7fhdr3gtdg1d91jur8ynxni0fcd2lhb1s283hxtjkzhzxxodvyd1hg4n1stf2b64wn&name=image-n9Nw7NcM9Pu1VaEIfj.jpg&dsid=211fef2b1e58f805d47e7de4c438d193",
-    icon: "https://docviewer.yandex.ru/view/682023045/htmlimage?id=1l6-in8kf6tbuhojxpscexvlfsuzsqr7j8l9qgjgsx4glzm45olqn5wcjt7xd2b4nn3ae4l8pswymcfdvutvhq4ftnurdket3d0p797&name=image-faQEx9jPsYjss8WeXB.png&dsid=cab8888a11a3aeb7cbb3cceb16c52aaf",
+    image: azmodanImage,
+    icon: rdd2,
     isInFreeRotation: false
   },
   {
@@ -22,8 +36,8 @@ export const mockHeroesData = [
     name: "Аларак",
     role: "melee-assassin",
     complexity: "very hard",
-    image: "https://docviewer.yandex.ru/view/682023045/htmlimage?id=4m7i-v7v9l5ca4jja7olrjbv6mss2p1db3bq3wonq2jvsyx6ydxmsrtepok8tetlujyro4rxq4tbthnopugsyjy43ie5g23ijhgc6gg&name=image-SqjJ4W34RdZeyiixB2.jpg&dsid=6d76e0441e7e2e37d38c6efafab708fa",
-    icon: "https://docviewer.yandex.ru/view/682023045/htmlimage?id=142-660zbu1dzwcbrpfs2oo4nfpefnt0k1pl9qsrph52kwrjdu0j07shb33phsma1pp9b19ultnnimyehktkc85n668l98k6tefmj4v&name=image-j61ooOme04DC9Qe9p4.png&dsid=2b744add4777306e43115d03745bfefa",
+    image: alarakImage,
+    icon: mdd5,
     isInFreeRotation: true
   },
   {
@@ -31,8 +45,8 @@ export const mockHeroesData = [
     name: "Алекстраза",
     role: "healer",
     complexity: "hard",
-    image: "https://docviewer.yandex.ru/view/682023045/htmlimage?id=8zr6-f79b3zxm2cvdhlt7wpq6voofyym258z73nxzwdx2vxeqfz0ryssc1m0lrbrehusx5ny5l0o6pnb07u73eth2gc6sgxvgrtnzvia&name=image-RzZQHmLS43IIjCFXMF.jpg&dsid=b7a5d9ab92fa8323eeb0711f3124ae59",
-    icon: "https://docviewer.yandex.ru/view/682023045/htmlimage?id=19m-4a109e8utgos4zhbvvxc3sh0sr6qge54vla7r3enuvr8qlqt3zu5uuig4o0qkm69yys6txc0pkgr0dzizicu4kteez2kgtr85ua&name=image-B3jzA7oMojHUe44IEA.png&dsid=22d3362c745f363e5eafe9c9a3d3a155",
+    image: alextraszaImage,
+    icon: healer4,
     isInFreeRotation: false
   },
   {
@@ -40,8 +54,8 @@ export const mockHeroesData = [
     name: "Ана",
     role: "healer",
     complexity: "very hard",
-    image: "https://docviewer.yandex.ru/view/682023045/htmlimage?id=5jft-e26qfziz75l3aaaq9puyh97b3yrpjx0sptmv3m97eck72uu5f1y3qu4hgjrvqpdf5adjl07u5l91p0oknfjc4xzs6tiwopgvwtu&name=image-VkNRMTXLipZj72JzM8.jpg&dsid=158058206408e6721a9e99f78d88148e",
-    icon: "https://docviewer.yandex.ru/view/682023045/htmlimage?id=13e-166tha8jnthiupnuv0g71rnq4qfvevsst8m133u7bvz97tfvovpsx5hxq9k2iowe9hg3gp8unazri29lf60xemv0vemqjbo233c&name=image-PxuhfMhCFp2E35Peih.png&dsid=61bb743315c740dc098998543116c166",
+    image: anaImage,
+    icon: healer5,
     isInFreeRotation: true
   },
   {
@@ -49,8 +63,8 @@ export const mockHeroesData = [
     name: "Андуин",
     role: "healer",
     complexity: "easy",
-    image: "https://docviewer.yandex.ru/view/682023045/htmlimage?id=81s6-7mmmmto0m1twmcor98m8qd7bedpavub2824artpv0ugw12q5yonzl5wlbahfr3szhlzuphi7npcmk9acdv84ecpirg53py7cogk&name=image-cc416rJBYcLLfK1j2W.jpg&dsid=501ca2ba55152480c2587d89062341ca",
-    icon: "https://docviewer.yandex.ru/view/682023045/htmlimage?id=19j-jc1a8uwnxzjrma8iq9g9na7hdweha5acveih3sndqcdyv48y8kj70f09sr8jemnhrpx8kcywi1nuj9yzrtyed5ewzbfg421qjrr&name=image-uS9ljYYsfuCjliJIiQ.png&dsid=3aa0e005e09fc8dbd8e8c6683c68d992",
+    image: anduinImage,
+    icon: healer2,
     isInFreeRotation: false
   },
 ]
